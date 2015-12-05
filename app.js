039bec00a2b47a5503e66c18a17b5508144afc5d
alex5il <alexkono91@gmail.com>
2015-12-05 13:44:40 +0200

    Basic stuff really

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+var server = app.listen(8080, function () {
+  var host = "localhost";
+  var port = server.address().port;
+  
+  console.log("Server started");
+});
