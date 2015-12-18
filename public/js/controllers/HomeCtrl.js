49afc752090fa216421e52b0f469f553af7014b6
BlichMike <blich.mike@gmail.com>
2015-12-18 13:44:48 +0200

    Delete HomeCtrl.js

--- public/js/controllers/HomeCtrl.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Mike on 05/12/2015.
- */
-var tripUsControllers = angular.module('tripUsControllers', []);
-tripUsControllers.controller('homeCtrl', ['$scope', '$uibModal', 'group',
-    function ($scope, $modal, group) {
-    }
-]);
