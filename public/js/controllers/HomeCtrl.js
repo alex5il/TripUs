84df710685089bb066f51c1f160c70e3cf3a2a81
BlichMike <blich.mike@gmail.com>
2015-12-18 13:39:00 +0200

    asd

---
+++ public/js/controllers/HomeCtrl.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Mike on 05/12/2015.
+ */
+var tripUsControllers = angular.module('tripUsControllers', []);
+tripUsControllers.controller('homeCtrl', ['$scope', '$uibModal', 'group',
+    function ($scope, $modal, group) {
+    }
+]);
