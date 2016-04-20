fb04e13144e2d69ba12fa4107cecc92b3b483b61
sergio <sergeyalh1@gmail.com>
2016-04-20 19:38:29 +0300

    alex gui

--- public/js/controllers/requirementsCtrl.js
+++ public/js/controllers/requirementsCtrl.js
@@ -1,8 +1,11 @@
 tripUsControllers.controller('requirementsCtrl',
     ['$scope', '$location','$routeParams' , 'group', function ($scope, $location, $routeParams, group) {
             var myGroup = group.getGroup($routeParams.groupId);
-            console.log(myGroup.name);
             $scope.requirements = ['a', 'b', 'c', 'd'];
+
+        // Star rating crap
+        console.log("asdasd ");
+        $("#input-id").rating();
         }
     ]
 );
