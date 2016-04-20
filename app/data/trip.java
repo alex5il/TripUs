97af5dff6686c688b4217cc57eb385f6330490f7
sergio <sergeyalh1@gmail.com>
2016-04-20 17:43:53 +0300

    DB !!!!!

--- app/data/trip.java
+++ app/data/trip.java
@@ -1,39 +1,91 @@
 package data;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import org.jongo.MongoCollection;
+import org.jongo.marshall.jackson.oid.MongoObjectId;
 import uk.co.panaxiom.playjongo.PlayJongo;
 import play.*;
-import org.jongo.*;
 
 /**
  * Created by sergio on 08/04/2016.
  */
-public class trip {
-
+public class Trip {
     public static PlayJongo jongo = Play.application().injector().instanceOf(PlayJongo.class);
 
-    public static MongoCollection countries() {
-        return jongo.getCollection("countries");
+    public static MongoCollection trips() {
+        return jongo.getCollection("trips");
     }
 
-    @JsonProperty("_id")
-    public String id;
+    // auto
+    @MongoObjectId
+    private String _id;
 
-    public String name;
+    private String tripLeader, tripName, key;
+    private User[] users;
+    private Pick[] picks;
+
+    public Trip() {}
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getTripLeader() {
+        return tripLeader;
+    }
+
+    public void setTripLeader(String tripLeader) {
+        this.tripLeader = tripLeader;
+    }
+
+    public String getTripName() {
+        return tripName;
+    }
+
+    public void setTripName(String tripName) {
+        this.tripName = tripName;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public User[] getUsers() {
+        return users;
+    }
+
+    public void setUsers(User[] users) {
+        this.users = users;
+    }
+
+    public Pick[] getPicks() {
+        return picks;
+    }
+
+    public void setPicks(Pick[] picks) {
+        this.picks = picks;
+    }
 
     public void insert() {
-        countries().save(this);
+        trips().save(this);
     }
 
     public void remove() {
-        countries().remove(this.id);
+        trips().remove(this._id);
     }
 
-    public static trip findByName(String name) {
-        return countries().findOne("{name: #}", name).as(trip.class);
+    public static Trip findByKey(String key) {
+        return trips().findOne("{key: #}", key).as(Trip.class);
     }
 
-    public static trip findByID(String id) {
-        return countries().findOne("{_id: #}", id).as(trip.class);
+    public static Trip findByTripLeader(String tripLeader) {
+        return trips().findOne("{tripLeader: #}", tripLeader).as(Trip.class);
     }
 }
