cb9a8fb2e1395085b0681ffd7f81b2a87ca8ce2b
sergio <sergeyalh1@gmail.com>
2016-04-22 14:59:02 +0300

    algo for anton

--- app/controllers/AlgorithmController.java
+++ app/controllers/AlgorithmController.java
@@ -1,14 +1,12 @@
 package controllers;
 
-import data.Trip;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class AlgorithmController extends Controller {
     public Result index() {
         //System.out.println(Trip.trips().find().as(Trip.class).next());
-        System.out.println(Trip.findByTripLeader("Anton"));
-
+        System.out.println("anton !!! ");
         return ok();
     }
 
