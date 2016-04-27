868b985059c71590e61c5f106b3118e06eda6117
RoboticsAlmaliah2015 <robotics@spam4.me>
2016-04-27 12:07:52 +0300

    Genetic algorithm

---
+++ app/data/Point.java
@@ -0,0 +1,57 @@
+package data;
+
+import org.jongo.MongoCollection;
+import org.jongo.MongoCursor;
+import org.jongo.marshall.jackson.oid.MongoObjectId;
+import play.Play;
+import uk.co.panaxiom.playjongo.PlayJongo;
+
+import java.util.Random;
+
+public class Point {
+    public static PlayJongo jongo = Play.application().injector().instanceOf(PlayJongo.class);
+
+    @MongoObjectId
+    private String _id;
+    private Properties properties;
+    private Geometry geometry;
+
+    public static MongoCollection getPointsCollection() {
+        return PlayJongo.getCollection("points");
+    }
+
+    public static MongoCursor<Point> getPointsCursor() {
+        return getPointsCollection().find().as(Point.class);
+    }
+
+    public static MongoCursor<Point> getRandomPoints(int quantity){
+        int totalPoints = (int)getPointsCollection().count();
+        Random rnd = new Random();
+
+        return getPointsCollection().find().limit(quantity).skip(rnd.nextInt(totalPoints - quantity)).as(Point.class);
+    }
+
+    public String getId() {
+        return this._id;
+    }
+
+    public long getOsmId() {
+        return this.properties.getOsmId();
+    }
+
+    public String getName() {
+        return this.properties.getName();
+    }
+
+    public String getAmenity() {
+        return this.properties.getAmenity();
+    }
+
+    public double getLatitude() {
+        return this.geometry.getLatitude();
+    }
+
+    public double getLongitude() {
+        return this.geometry.getLongitude();
+    }
+}
