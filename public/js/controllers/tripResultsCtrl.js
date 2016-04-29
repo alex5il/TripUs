cb6a3804c0d3f794f2728f5a0f02cf1f754b97ba
blitshtain <blich.mike@gmail.com>
2016-04-29 17:35:31 +0300

    Setting markers to all path points + animation on start marker

--- public/js/controllers/tripResultsCtrl.js
+++ public/js/controllers/tripResultsCtrl.js
@@ -2,6 +2,18 @@ tripUsControllers.controller('tripResultsCtrl', function ($scope, TripResults, $
 
     $scope.polylines = [];
     $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+    $scope.setPathMarkers = function() {
+        $scope.pathMarkers = [];
+        angular.forEach($scope.currentPath.path, function (point, index) {
+                    $scope.pathMarkers.push(
+                        {
+                            id: index,
+                            coords: {longitude: point.longitude, latitude: point.latitude},
+                            options: {labelContent: (point.name && point.name != 'null') ? point.name : '', labelAnchor: '0 0', labelClass: 'anchorLabel', animation: index == 0? 1 : 0}
+                        }
+                    );
+                });
+    };
     TripResults.get($routeParams.groupId).then(function (result) {
         $scope.tripResults = result;
         angular.forEach($scope.tripResults, function (tripResult, index) {
@@ -17,16 +29,7 @@ tripUsControllers.controller('tripResultsCtrl', function ($scope, TripResults, $
             });
         });
 
-        $scope.currentPath = $scope.polylines[1];
-        $scope.pathMarkers = [];
-        angular.forEach($scope.currentPath.path, function (point, index) {
-            $scope.pathMarkers.push(
-                {
-                    id: index,
-                    coords: {longitude: point.longitude, latitude: point.latitude},
-                    options: {labelContent: point.name || '', labelAnchor: '0 0', labelClass: 'anchorLabel', animation: index == 0? 1 : 0}
-                }
-            );
-        });
+        $scope.currentPath = $scope.polylines[0];
+        $scope.setPathMarkers();
     });
 });
